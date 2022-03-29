6c48fea19b1402e79d5e9f34c7f29c42b9583f8c
nurhidayati0901 <75291838+nurhidayati0901@users.noreply.github.com>
2022-03-30 04:57:50 +0700

    no 2

--- client-its.py
+++ client-its.py
@@ -3,30 +3,35 @@ import ssl
 import re
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('its.ac.id', 443)
-client_socket.connect(server_address)
+server_address = ('www.its.ac.id', 443)
 client_socket = ssl.wrap_socket(client_socket, keyfile=None, 
                 certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, 
                 ssl_version=ssl.PROTOCOL_SSLv23)
+client_socket.connect(server_address)
 
-request_header = b"GET https://www.its.ac.id/ HTTP/1.1\r\nHost: its.ac.id\r\nConnection: close\r\n\r\n"
-client_socket.send(request_header)
+request_header = ('GET / HTTP/1.1\r\nHost: www.its.ac.id\r\nConnection: close\r\nAccept: text/html\r\nAccept-Encoding: gzip, deflate, br\r\n\r\n').encode('utf-8')
+client_socket.sendall(request_header)
 
-response = ''
+response = b''
 while True:
     received = client_socket.recv(1024)
     if not received:
         break
-    response += received.decode('utf-8')
+    response += received
 
-# print(response)
-response_header = response.split('\r\n\r\n')[0]
-print(response_header)
+# print(response[:572].decode('utf-8'))
+response_header = response[:572].decode('utf-8')
+# response_header = response.split('\r\n\r\n')[0]
+# print(response_header)
 
 # no 1
 string = response_header.split('\r\n')[0]
 status_code = re.search(r'([1-5][0-9][0-9].*)', string).group(1)
 print(status_code)
+
+# no 2
+encoding = re.search(r'\bContent-Encoding: (.*)\n\b', response_header).group(1)
+print(encoding)
 
 # no 3
 protocol = re.search(r'(\b[A-Za-z0-9./]* \b)', string).group(1)
